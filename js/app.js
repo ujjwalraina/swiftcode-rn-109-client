6c9281b9b304b89c4fd1f548326e6104cbbb51fd
Live session user <sandeep.k@betsol.com>
2016-10-24 11:22:51 +0000

    finalClient

--- js/app.js
+++ js/app.js
@@ -76,4 +76,64 @@ var URL = "http://betsol.org:9000/";app.config(function($routeProvider){
      });
    }
 }]);app.controller('dashboardCtrl',['$scope','$location','$http',
- function(){  }]);
+ function($scope,$location,$http){
+   $scope.getprofileData = function(){
+     var request = $http({
+       method: "GET",
+       url:    URL + "profile/"+sessionStorage.userId
+     });request.success(function(data){
+       console.log(data);
+       $scope.profileData=angular.fromJson(data);
+     });
+       request.error(function(data){
+         console.log(data);
+       });
+     }
+     $scope.getprofileData();
+     $scope.updateprofile = function(){
+       delete $scope.profileData["connectionRequests"];
+        delete $scope.profileData["connections"];
+         delete $scope.profileData["suggestions"];
+       var request = $http({
+         method: "PUT",
+         url:    URL + "profile/"+sessionStorage.userId,
+         data: $scope.profileData
+       });request.success(function(data){
+         $scope.responseMessage = "update successfull.";
+         $("#dashboardMsgModal").modal('show');
+         $scope.getprofileData();
+                   console.log(data);
+       });
+         request.error(function(data){
+           console.log(data);
+         });
+       }
+       $scope.sendConnectionRequest = function(receiverId){
+         var request = $http({
+           method: "POST",
+           url:    URL + "request/send/"+sessionStorage.userId+"/"+receiverId,
+         });
+         request.success(function(data){
+           $scope.responseMessage = "Your request has been sent.";
+           $("#dashboardMsgModal").modal('show');
+           $scope.getprofileData();
+         });
+           request.error(function(data){
+             console.log(data);
+           });
+       }
+       $scope.acceptConnectionRequest = function(requestId){
+         var request = $http({
+           method: "POST",
+           url:    URL + "request/accept/"+requestId,
+         });
+         request.success(function(data){
+           $scope.responseMessage = "Your request has been confirmed.";
+           $("#dashboardMsgModal").modal('show');
+           $scope.getprofileData();
+         });
+           request.error(function(data){
+             console.log(data);
+           });
+       }
+ }]);
